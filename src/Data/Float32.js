8b75359ff59c06aa85c28e4281d5f1bf8bd072be
Athan Clark <athan.clark@localcooking.com>
2019-01-19 06:21:24 -0700

    better browser compatability

--- src/Data/Float32.js
+++ src/Data/Float32.js
@@ -1,5 +1,11 @@
 "use strict";
 
 exports.fromNumber = function fromNumber (x) {
+  if (typeof Math.fround !== 'undefined') {
     return Math.fround(x);
+  } else {
+    var r = new Float32Array(1);
+    r[0] = s;
+    return r[0];
+  }
 };
