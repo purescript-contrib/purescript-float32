534d9d9af02c13e8d06e462d2d7cc842c6d1b190
Athan Clark <athan.clark@localcooking.com>
2019-01-18 23:00:08 -0700

    gen

---
+++ src/Data/Float32/Gen.purs
@@ -0,0 +1,9 @@
+module Data.Float32.Gen where
+
+import Prelude ((<$>))
+import Data.Float32 (Float32, fromNumber, toNumber)
+import Control.Monad.Gen.Class (class MonadGen, chooseFloat)
+
+
+chooseFloat32 :: forall m. MonadGen m => Float32 -> Float32 -> m Float32
+chooseFloat32 a b = fromNumber <$> chooseFloat (toNumber a) (toNumber b)
